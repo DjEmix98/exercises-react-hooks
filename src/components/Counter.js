89695942c532402642f2d8c03930f73d4fff3cca
Emmanuel-Bellissimo <118261948+Emmanuel-Bellissimo@users.noreply.github.com>
2022-12-31 00:36:46 +0100

    refactor css classes

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import "../assets/counter.css";
 import "bootstrap";
 export function Counter() {
   const [counter, setCounter] = useState(0);
   return (
-    <div className="counter__container">
-      <section>
-        <h4 className="counter__title mb-3">Exercise 1 - Counter</h4>
+    <div className="exercise__container">
+      <section className="mb-4">
+        <h4 className="exercise__sub-title mb-3">Exercise 1 - Counter</h4>
         <button onClick={() => setCounter(counter + 1)}>+</button> {counter}{" "}
         <button onClick={() => setCounter(counter - 1)}>-</button>
       </section>
